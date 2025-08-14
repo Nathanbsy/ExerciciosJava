2aa3724b0bdef3e695c70b44e87b58b9b1ee5fb0
Nathanbsy <nathanbs1227@gmail.com>
2025-08-14 11:21:29 -0300

    testando a linguagem

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,17 @@
 public class Main {
     public static void main(String[] args) {
         int quantidadePassos = 500;
-        double alturaEmMetros = 1.60;
-        String nome = "";
-        int alturaEmCentimetros = (int) (alturaEmMetros * 100);
-        System.out.println(alturaEmCentimetros);
+        double alturaEmMetros = 1.50;
+        String nome = "Thayná";
+//        int alturaEmCentimetros = 150;
+//        alturaEmMetros = alturaEmCentimetros;
+//        System.out.println(alturaEmMetros / 100);
+        String sugestao = "";
+        if(quantidadePassos < 100) {
+            sugestao = "Sugiro que você tente aumentar a meta!";
+            System.out.println(sugestao);
+        } else {
+            sugestao = "Parabéns";
+        }
     }
 }
